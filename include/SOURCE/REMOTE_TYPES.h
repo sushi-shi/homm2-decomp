b97f6f5d9056c93347fdbd64fbf4eff58b248702
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-14 17:48:10 +0000

    match: reconstruct REMOTE transport

---
+++ include/SOURCE/REMOTE_TYPES.h
@@ -0,0 +1,23 @@
+#ifndef HOMM2_REMOTE_TYPES_H
+#define HOMM2_REMOTE_TYPES_H
+
+#include <va.h>
+
+typedef enum NetPlayerInfoConstant {
+    NET_PLAYER_INFO_NAME_SIZE = 21,
+    NET_PLAYER_INFO_RESERVED_SIZE = 6
+} NetPlayerInfoConstant;
+
+#pragma pack(push, 1)
+struct SNetPlayerInfo {
+    int networkId;
+    char name[NET_PLAYER_INFO_NAME_SIZE];
+    signed char connectionType;
+    signed char useRegularCompression;
+    signed char useDiffCompression;
+    char reserved[NET_PLAYER_INFO_RESERVED_SIZE];
+};
+#pragma pack(pop)
+SIZE(SNetPlayerInfo, 34);
+
+#endif // HOMM2_REMOTE_TYPES_H
