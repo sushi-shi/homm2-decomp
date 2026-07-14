38bfdbe3b0eeae6588d6adfbb537803bf9798b59
homm2-decomp <decomp@homm2>
2026-07-14 12:42:02 +0000

    match: isolate icon2bc dim palette surface

---
+++ include/SOURCE/dimPalette.h
@@ -0,0 +1,7 @@
+#ifndef HOMM2_SOURCE_DIMPALETTE_H
+#define HOMM2_SOURCE_DIMPALETTE_H
+
+// Shared dimming lookup table owned and defined by SOURCE/KB.
+extern unsigned char uDimPal[3][4][256];
+
+#endif
