8c8e46d9e62a0f4c1303b3f55aca96d92c047509
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-15 06:42:46 +0000

    match: recover FlipDimIconToBitmap domains

--- include/BASE/IconMonoRle.h
+++ include/BASE/IconMonoRle.h
@@ -3,6 +3,9 @@
 
 // Serialized run-length domain used by monochrome icon streams.
 typedef enum IconMonoRleCommandConstant {
+    ICON_RLE_MONO_NEWLINE_COMMAND = 0x00,
+    ICON_RLE_MONO_END_COUNT = 0x00,
+    ICON_RLE_MONO_SKIP_FLAG = 0x80,
     ICON_RLE_MONO_RUN_MASK = 0x7f
 } IconMonoRleCommandConstant;
 
