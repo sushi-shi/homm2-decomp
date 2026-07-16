1ca4016486d3a21b513bd3132a74648b86d7ea1f
homm2-decomp <decomp@homm2>
2026-07-16 06:29:39 +0000

    data: recover FINDPATH storage topology

---
+++ include/SOURCE/FINDPATH_TYPES.h
@@ -0,0 +1,23 @@
+#ifndef HOMM2_SOURCE_FINDPATH_TYPES_H
+#define HOMM2_SOURCE_FINDPATH_TYPES_H
+
+#include <va.h>
+
+#define FINDPATH_SOURCE_FILE "I:\\Projects\\Heroes\\Prog\\SOURCE\\FINDPATH.CPP"
+
+#define FINDPATH_ALLOCATION_SOURCE_LINE_BASE 20
+#define FINDPATH_DESTRUCTION_SOURCE_LINE_BASE 26
+
+struct SFindPathSourceLine {
+    short value;
+    short reserved;
+};
+SIZE(SFindPathSourceLine, 0x4);
+
+struct SFindPathSourceLocation {
+    SFindPathSourceLine line;
+    char sourceFile[sizeof(FINDPATH_SOURCE_FILE)];
+};
+SIZE(SFindPathSourceLocation, 0x30);
+
+#endif // HOMM2_SOURCE_FINDPATH_TYPES_H
