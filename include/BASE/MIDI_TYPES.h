db7c8f38f9992b38e32ad7951197cbc367a06e12
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-16 05:02:41 +0000

    data: recover Midi text storage

---
+++ include/BASE/MIDI_TYPES.h
@@ -0,0 +1,26 @@
+#ifndef HOMM2_BASE_MIDI_TYPES_H
+#define HOMM2_BASE_MIDI_TYPES_H
+
+#include <va.h>
+
+typedef enum MidiTextSlotSize {
+    MIDI_TEXT_SHORT_SLOT_SIZE = 0x4,
+    MIDI_TEXT_LOG_SLOT_SIZE = 0x8,
+    MIDI_TEXT_FILENAME_FORMAT_SLOT_SIZE = 0x14
+} MidiTextSlotSize;
+
+typedef struct SMidiText {
+    char startupBegin[MIDI_TEXT_SHORT_SLOT_SIZE];
+    char startupDriver[MIDI_TEXT_SHORT_SLOT_SIZE];
+    char startupOpen[MIDI_TEXT_LOG_SLOT_SIZE];
+    char startupOpenResult[MIDI_TEXT_LOG_SLOT_SIZE];
+    char shutdownBegin[MIDI_TEXT_SHORT_SLOT_SIZE];
+    char shutdownDriver[MIDI_TEXT_SHORT_SLOT_SIZE];
+    char shutdownComplete[MIDI_TEXT_SHORT_SLOT_SIZE];
+    char playBegin[MIDI_TEXT_LOG_SLOT_SIZE];
+    char filenameFormat[MIDI_TEXT_FILENAME_FORMAT_SLOT_SIZE];
+} SMidiText;
+
+SIZE(SMidiText, 0x40);
+
+#endif
