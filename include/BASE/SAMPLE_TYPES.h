ede196d68d16c6db620bb04e69082749645815e9
homm2-decomp <decomp@homm2>
2026-07-16 04:42:30 +0000

    data: recover SAMPLE allocation provenance

---
+++ include/BASE/SAMPLE_TYPES.h
@@ -0,0 +1,16 @@
+#ifndef HOMM2_BASE_SAMPLE_TYPES_H
+#define HOMM2_BASE_SAMPLE_TYPES_H
+
+#include <va.h>
+
+#define SAMPLE_SOURCE_FILE "I:\\Projects\\Heroes\\Prog\\BASE\\SAMPLE.CPP"
+
+struct SSampleSourceFiles {
+    char sampleAllocation[sizeof(SAMPLE_SOURCE_FILE)];
+    char sampleDestruction[sizeof(SAMPLE_SOURCE_FILE)];
+    char midiAllocation[sizeof(SAMPLE_SOURCE_FILE)];
+    char midiDestruction[sizeof(SAMPLE_SOURCE_FILE)];
+};
+SIZE(SSampleSourceFiles, 0xa0);
+
+#endif // HOMM2_BASE_SAMPLE_TYPES_H
