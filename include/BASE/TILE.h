6257353b2589c3cb13f312be93fc694eaed8084d
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-19 16:54:14 +0000

    cleanup: name tile blitter constants

--- include/BASE/TILE.h
+++ include/BASE/TILE.h
@@ -6,6 +6,12 @@
 class bitmap;
 class tileset;
 
+H2_ENUM_BEGIN(TileFlag)
+    TILE_INDEX_MASK      = 0x0fff,
+    TILE_FLIP_VERTICAL   = 0x4000,
+    TILE_FLIP_HORIZONTAL = 0x8000
+H2_ENUM_END(TileFlag)
+
 extern "C" void __cdecl TileToBitmap(tileset* src, u32 flags, bitmap* dst, i32 x, i32 y);
 
 #endif
