83f5bb47a7a5949b0b46689e74a921b1fcf1b0d5
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-20 21:28:11 +0000

    cleanup: type expansion campaign maps

--- include/SOURCE/ExpCampaign.h
+++ include/SOURCE/ExpCampaign.h
@@ -15,33 +15,36 @@ H2_ENUM_CLASS_BEGIN(ExpansionCampaignId)
     EXPANSION_CAMPAIGN_COUNT            = 4
 H2_ENUM_CLASS_END(ExpansionCampaignId)
 
-H2_ENUM_BEGIN(ExpansionCampaignMap)
-    MAP_NONE                  = -1,
-    MAP_POL_UPRISING          = 0,
-    MAP_POL_ISLAND_OF_CHAOS   = 1,
-    MAP_POL_ARROWS_FLIGHT     = 2,
-    MAP_POL_ABYSS             = 3,
-    MAP_POL_GIANTS_PASS       = 4,
-    MAP_POL_AURORA_BOREALIS   = 5,
-    MAP_POL_BETRAYALS_END     = 6,
-    MAP_POL_CORRUPTIONS_HEART = 7,
-    MAP_DES_CONQUER_AND_UNIFY = 0,
-    MAP_DES_BORDER_TOWNS      = 1,
-    MAP_DES_WAYWARD_SON       = 2,
-    MAP_DES_UNCLE_IVAN        = 3,
-    MAP_DES_SOUTHERN_WAR      = 4,
-    MAP_DES_IVORY_GATES       = 5,
-    MAP_DES_ELVEN_LANDS       = 6,
-    MAP_DES_EPIC_BATTLE       = 7,
-    MAP_WIZ_SHROUDED_ISLES    = 0,
-    MAP_WIZ_ETERNAL_SCROLLS   = 1,
-    MAP_WIZ_POWERS_END        = 2,
-    MAP_WIZ_FOUNT_OF_WIZARDRY = 3,
-    MAP_VOY_STRANDED          = 0,
-    MAP_VOY_PIRATE_ISLES      = 1,
-    MAP_VOY_KING_AND_COUNTRY  = 2,
-    MAP_VOY_BLOOD_IS_THICKER  = 3
-H2_ENUM_END(ExpansionCampaignMap)
+H2_ENUM_CLASS_BEGIN(ExpansionCampaignMap)
+    MAP_NONE                                    = -1,
+    MAP_FIRST                                   = 0,
+    MAP_POL_UPRISING                            = 0,
+    MAP_POL_ISLAND_OF_CHAOS                     = 1,
+    MAP_POL_ARROWS_FLIGHT                       = 2,
+    MAP_POL_ABYSS                               = 3,
+    MAP_POL_GIANTS_PASS                         = 4,
+    MAP_POL_AURORA_BOREALIS                     = 5,
+    MAP_POL_BETRAYALS_END                       = 6,
+    MAP_POL_CORRUPTIONS_HEART                   = 7,
+    MAP_DES_CONQUER_AND_UNIFY                   = 0,
+    MAP_DES_BORDER_TOWNS                        = 1,
+    MAP_DES_WAYWARD_SON                         = 2,
+    MAP_DES_UNCLE_IVAN                          = 3,
+    MAP_DES_SOUTHERN_WAR                        = 4,
+    MAP_DES_IVORY_GATES                         = 5,
+    MAP_DES_ELVEN_LANDS                         = 6,
+    MAP_DES_EPIC_BATTLE                         = 7,
+    MAP_WIZ_SHROUDED_ISLES                      = 0,
+    MAP_WIZ_ETERNAL_SCROLLS                     = 1,
+    MAP_WIZ_POWERS_END                          = 2,
+    MAP_WIZ_FOUNT_OF_WIZARDRY                   = 3,
+    MAP_VOY_STRANDED                            = 0,
+    MAP_VOY_PIRATE_ISLES                        = 1,
+    MAP_VOY_KING_AND_COUNTRY                    = 2,
+    MAP_VOY_BLOOD_IS_THICKER                    = 3,
+    EXPANSION_CAMPAIGN_FIRST_ALTERNATE_NAME_MAP = 4
+H2_ENUM_CLASS_END(ExpansionCampaignMap)
+H2_ENUM_STEPPED(ExpansionCampaignMap)
 
 H2_ENUM_CLASS_BEGIN(ExpansionCampaignAward)
     AWARD_ELVEN_ALLIANCE      = 0,
@@ -58,36 +61,35 @@ H2_ENUM_CLASS_BEGIN(ExpansionCampaignAward)
 H2_ENUM_CLASS_END(ExpansionCampaignAward)
 
 H2_ENUM_BEGIN(ExpansionCampaignConstant)
-    EXPANSION_CAMPAIGN_MAX_MAP_COUNT            = 8,
-    EXPANSION_CAMPAIGN_BONUS_CHOICE_COUNT       = 3,
-    EXPANSION_CAMPAIGN_AWARD_COUNT              = 11,
-    EXPANSION_CAMPAIGN_RUNTIME_GAP_SIZE         = 4,
-    EXPANSION_CAMPAIGN_PLAYER_SETUP_RESET_SIZE  = 0x41,
-    EXPANSION_CAMPAIGN_ARMY_NAME_BUFFER_SIZE    = 52,
-    EXPANSION_CAMPAIGN_HERO_COUNT               = 54,
-    EXPANSION_CAMPAIGN_TRACK_ICON_SIZE          = 41,
-    EXPANSION_CAMPAIGN_TRACK_ICON_COLOR         = 0x10,
-    EXPANSION_CAMPAIGN_MAP_FILENAME_SIZE        = 13,
-    EXPANSION_CAMPAIGN_SPECIAL_SKILL_LEVEL      = 1,
-    EXPANSION_CAMPAIGN_SPECIAL_SKILL            = 12,
-    EXPANSION_CAMPAIGN_SPECIAL_SKILL_ALT_LEVEL  = 2,
-    EXPANSION_CAMPAIGN_SPECIAL_SKILL_ALT        = 2,
-    EXPANSION_CAMPAIGN_ICON_FRAME_BASE          = 15,
-    EXPANSION_CAMPAIGN_MUSIC                    = 24,
-    EXPANSION_CAMPAIGN_DAYS_PER_WEEK            = 7,
-    EXPANSION_CAMPAIGN_DAYS_PER_MONTH           = 28,
-    EXPANSION_CAMPAIGN_FIRST_ALTERNATE_NAME_MAP = 4,
-    EXPANSION_CAMPAIGN_IVAN_NAME_OFFSET         = 6,
-    EXPANSION_CAMPAIGN_MAIN_PLAYER              = 0,
-    EXPANSION_CAMPAIGN_GOLDEN_BOW_EVENT_X       = 5,
-    EXPANSION_CAMPAIGN_GOLDEN_BOW_EVENT_Y       = 0
+    EXPANSION_CAMPAIGN_MAX_MAP_COUNT           = 8,
+    EXPANSION_CAMPAIGN_BONUS_CHOICE_COUNT      = 3,
+    EXPANSION_CAMPAIGN_AWARD_COUNT             = 11,
+    EXPANSION_CAMPAIGN_RUNTIME_GAP_SIZE        = 4,
+    EXPANSION_CAMPAIGN_PLAYER_SETUP_RESET_SIZE = 0x41,
+    EXPANSION_CAMPAIGN_ARMY_NAME_BUFFER_SIZE   = 52,
+    EXPANSION_CAMPAIGN_HERO_COUNT              = 54,
+    EXPANSION_CAMPAIGN_TRACK_ICON_SIZE         = 41,
+    EXPANSION_CAMPAIGN_TRACK_ICON_COLOR        = 0x10,
+    EXPANSION_CAMPAIGN_MAP_FILENAME_SIZE       = 13,
+    EXPANSION_CAMPAIGN_SPECIAL_SKILL_LEVEL     = 1,
+    EXPANSION_CAMPAIGN_SPECIAL_SKILL           = 12,
+    EXPANSION_CAMPAIGN_SPECIAL_SKILL_ALT_LEVEL = 2,
+    EXPANSION_CAMPAIGN_SPECIAL_SKILL_ALT       = 2,
+    EXPANSION_CAMPAIGN_ICON_FRAME_BASE         = 15,
+    EXPANSION_CAMPAIGN_MUSIC                   = 24,
+    EXPANSION_CAMPAIGN_DAYS_PER_WEEK           = 7,
+    EXPANSION_CAMPAIGN_DAYS_PER_MONTH          = 28,
+    EXPANSION_CAMPAIGN_IVAN_NAME_OFFSET        = 6,
+    EXPANSION_CAMPAIGN_MAIN_PLAYER             = 0,
+    EXPANSION_CAMPAIGN_GOLDEN_BOW_EVENT_X      = 5,
+    EXPANSION_CAMPAIGN_GOLDEN_BOW_EVENT_Y      = 0
 H2_ENUM_END(ExpansionCampaignConstant)
 
 #pragma pack(push, 1)
 class ExpCampaign {
 public:
     ExpansionCampaignId m_campaignId;
-    ExpansionCampaignMap m_currentMap;
+    H2_ENUM_STORAGE(ExpansionCampaignMap, i32) m_currentMap;
     i32 m_mapCount;
     u8 m_mapChoices[EXPANSION_CAMPAIGN_MAX_MAP_COUNT];
     u8 m_mapsPlayed[EXPANSION_CAMPAIGN_MAX_MAP_COUNT];
@@ -95,7 +97,7 @@ public:
     u8 m_awards[EXPANSION_CAMPAIGN_AWARD_COUNT];
     u8 m_bonusChoices[EXPANSION_CAMPAIGN_MAX_MAP_COUNT];
     char m_pad_0x3f[EXPANSION_CAMPAIGN_RUNTIME_GAP_SIZE];
-    ExpansionCampaignMap m_viewMap;
+    H2_ENUM_STORAGE(ExpansionCampaignMap, i32) m_viewMap;
     class heroWindow* m_window;
     i32 m_viewOnly;
     char m_pad_0x4f;
@@ -131,7 +133,7 @@ private:
 
 public:
     void Autosave(void);
-    i32 Choose(void);
+    ExpansionCampaignId Choose(void);
     i16 Days(void);
     i32 CampaignID(void);
     char* JosephName(void);
