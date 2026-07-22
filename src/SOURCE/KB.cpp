12f3ee31cda1862f5fbfcd96c3ef7d4f762ddb9c
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-22 03:34:40 +0000

    cleanup: type serialized enum domains

--- src/SOURCE/KB.cpp
+++ src/SOURCE/KB.cpp
@@ -9957,7 +9957,7 @@ DATA(0x00528928) i32 gIndex;
 DATA(0x0052892c) i32 giWeekTypeExtra;
 DATA(0x00528930) philAI* gpPhilAI;
 DATA(0x00528934) i32 giTCPType;
-DATA(0x00528938) CombatEffectType gCurLoadedSpellEffect;
+DATA(0x00528938) H2_ENUM_STORAGE(CombatEffectType, i32) gCurLoadedSpellEffect;
 DATA(0x0052893c) class mouseManager* gpMouseManager;
 DATA(0x00528940) i32 giCurTempMobility;
 DATA(0x00528944) OverviewReturnAction giOverviewReturnAction;
