c11193163ed3a5bd1a312aa673e4d17399c71628
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-22 09:21:08 +0000

    match: stabilize CURSOR StopCursor exact match

--- src/SOURCE/CURSOR.cpp
+++ src/SOURCE/CURSOR.cpp
@@ -92,7 +92,10 @@ void advManager::StopCursor(i32 stopSound) {
     }
     m_cursorCycle = 0;
     if (m_previousCursorMapX != -1) {
-        m_mapData->GetCell(m_previousCursorMapX + m_mapOriginX, m_previousCursorMapY + m_mapOriginY)
+        m_mapData->GetCell(
+            OD_STEER(m_previousCursorMapX) + m_mapOriginX,
+            m_previousCursorMapY + m_mapOriginY
+        )
             ->m_flags &= ~CURSOR_MAP_VISIBLE_FLAG;
         m_previousCursorMapY = -1;
         m_previousCursorMapX = m_previousCursorMapY;
