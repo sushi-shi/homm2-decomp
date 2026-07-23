d59c6e55f1462586cc95866951f2ba355577530d
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-07-23 09:00:01 +0000

    match: reconstruct BASEMGR constructor

--- src/BASE/BASEMGR.cpp
+++ src/BASE/BASEMGR.cpp
@@ -3,8 +3,9 @@
 #include <string.h>
 
 VA(0x004d2530, 0x47)
-baseManager::baseManager(void) {
-    m_prev = m_next = NULL;
+baseManager::baseManager(void)
+    : m_next(NULL),
+      m_prev(NULL) {
     m_priority = BASE_MANAGER_PRIORITY_UNASSIGNED;
     m_messageMask = BASE_MANAGER_MESSAGE_MASK_ALL;
     m_active = false;
