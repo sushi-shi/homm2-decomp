fbcdde5b047111ee516eb90beae76c6c47a3def9
sushi-shi <47691267+sushi-shi@users.noreply.github.com>
2026-08-27 18:21:54 +0000

    build: verify native and cross targets

--- flake.nix
+++ flake.nix
@@ -157,6 +157,36 @@
 
       homm2 = mkNative false;
       homm2-debug = mkNative true;
+      homm2-check = homm2.overrideAttrs (_previous: {
+        doCheck = true;
+        checkPhase = ''
+          runHook preCheck
+          ctest --output-on-failure
+          runHook postCheck
+        '';
+      });
+
+      icon-check = pkgs.stdenv.mkDerivation {
+        pname = "homm2-icon-check";
+        version = "0.1.0";
+        src = source;
+        nativeBuildInputs = [ iconRust pkgs.clang ];
+        buildPhase = ''
+          runHook preBuild
+          export CARGO_HOME="$TMPDIR/cargo-home"
+          cd tools/homm2-icon-rs
+          cargo test --all-targets --locked
+          cargo test --doc --locked
+          cargo clippy --all-targets --locked -- -D warnings
+          cargo fmt --check
+          runHook postBuild
+        '';
+        installPhase = ''
+          runHook preInstall
+          touch "$out"
+          runHook postInstall
+        '';
+      };
 
       ironfist-revision = "314932011ed5308efb9f35cecc62e8ca638a7375";
       ironfist-source = pkgs.fetchgit {
@@ -362,6 +392,13 @@
         homm2-linux = homm2;
         homm2-windows = windows;
         default = homm2;
+      };
+
+      checks.${system} = {
+        native = homm2-check;
+        windows = windows;
+        web = homm2-web;
+        icon = icon-check;
       };
 
       apps.${system} = {
